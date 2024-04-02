7f03061c7d2955da5d4d63e1b32dd9d916e881d4
dashlexz1 <adr1999@icloud.com>
2024-04-03 02:30:39 +0300

    ScopeCommit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,27 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import java.util.logging.Logger;
 
 public class App {
     public static void main(String[] args) {
+        Logger log = Logger.getLogger(App.class.getName());
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        if(catBean == catBean2) {
+            log.info("true");
+        } else {
+            log.info("false");
+        }
+        if (bean == bean2) {
+            log.info("true");
+        } else {
+            log.info("false");
+        }
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,20 @@
+public class Cat {
+    private String name;
+    private String breed;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
